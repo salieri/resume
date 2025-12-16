4f741bb3941ff60a39b06738779760d2f966b921
Aleksi Asikainen <aleksi@abridge.com>
2025-12-15 17:51:03 -0800

    exec summary draft

---
+++ dev/apps/resume/src/sections/executive-summary/data.ts
@@ -0,0 +1,238 @@
+export interface ChartData {
+  label: string;
+  value: number;
+}
+
+export const technicalExperienceData: ChartData[] = [
+  {
+    label: 'Architecture',
+    value: 5,
+  },
+  {
+    label: 'Backend',
+    value: 5,
+  },
+  {
+    label: 'DevOps',
+    value: 4.5,
+  },
+  {
+    label: 'Frontend',
+    value: 4,
+  },
+  {
+    label: 'Mobile',
+    value: 3,
+  },
+  {
+    label: 'Testing/QA',
+    value: 4,
+  },
+  {
+    label: 'Technical Writing',
+    value: 5,
+  },
+];
+
+export const leadershipExperienceData: ChartData[] = [
+  {
+    label: 'Tech. Leadership',
+    value: 5,
+  },
+  {
+    label: 'Tech. Strategy',
+    value: 5,
+  },
+  {
+    label: 'Mgmt & Admin',
+    value: 3,
+  },
+  {
+    label: 'Recruitment',
+    value: 4,
+  },
+  {
+    label: 'Planning',
+    value: 5,
+  },
+  {
+    label: 'Mentoring & Training',
+    value: 4,
+  },
+  {
+    label: 'Product Development',
+    value: 5,
+  },
+];
+
+export const missionFitData: ChartData[] = [
+  {
+    label: 'Green-Field',
+    value: 5,
+  },
+  {
+    label: 'Next-Gen & 2.0',
+    value: 5,
+  },
+  {
+    label: 'Scaling',
+    value: 4.5,
+  },
+  {
+    label: 'Fix Projects',
+    value: 4,
+  },
+  {
+    label: 'Fix Teams',
+    value: 4,
+  },
+  {
+    label: 'Maintenance',
+    value: 2,
+  },
+  {
+    label: 'R&D',
+    value: 5,
+  },
+  {
+    label: 'Dev. Infra & Tooling',
+    value: 5,
+  },
+];
+
+export const focusFitData: ChartData[] = [
+  {
+    label: 'Architecture',
+    value: 5,
+  },
+  {
+    label: 'Strategy',
+    value: 5,
+  },
+  {
+    label: 'DevOps',
+    value: 4,
+  },
+  {
+    label: 'Collaboration',
+    value: 4,
+  },
+  {
+    label: 'Deep Work',
+    value: 5,
+  },
+  {
+    label: 'Mgmt & Admin',
+    value: 3,
+  },
+  {
+    label: 'Programming',
+    value: 5,
+  },
+];
+
+export const technologyFitData: ChartData[] = [
+  {
+    label: 'TypeScript',
+    value: 5,
+  },
+  {
+    label: 'Python',
+    value: 4,
+  },
+  {
+    label: 'MongoDB',
+    value: 4,
+  },
+  {
+    label: 'SQL',
+    value: 5,
+  },
+  {
+    label: 'Linux',
+    value: 5,
+  },
+  {
+    label: 'Electron',
+    value: 4,
+  },
+  {
+    label: 'Docker',
+    value: 5,
+  },
+  {
+    label: 'React',
+    value: 3.5,
+  },
+  {
+    label: 'Vue',
+    value: 4,
+  },
+  {
+    label: 'Angular',
+    value: 2,
+  },
+  {
+    label: 'NestJS',
+    value: 4,
+  },
+  {
+    label: 'GCP',
+    value: 4,
+  },
+  {
+    label: 'Azure',
+    value: 2,
+  },
+  {
+    label: 'AWS',
+    value: 4,
+  },
+];
+
+export const industryExperienceData: ChartData[] = [
+  {
+    label: 'B2C',
+    value: 5,
+  },
+  {
+    label: 'B2B',
+    value: 4,
+  },
+  {
+    label: 'Fintech',
+    value: 3.5,
+  },
+  {
+    label: 'Automotive',
+    value: 3.5,
+  },
+  {
+    label: 'HW/Embedded',
+    value: 2,
+  },
+  {
+    label: 'Home Automation',
+    value: 3,
+  },
+  {
+    label: 'Video Games',
+    value: 3,
+  },
+  {
+    label: 'Real-Time',
+    value: 4,
+  },
+  {
+    label: 'Payment Solutions',
+    value: 4,
+  },
+  {
+    label: 'Talent Search',
+    value: 4,
+  },
+  {
+    label: 'Healthcare',
+    value: 4,
+  },
+];
