692daccc6840a7a915b50dba805cff20a250b591
Aleksi Asikainen <aleksi@abridge.com>
2025-12-16 15:30:35 -0800

    i18n titles

--- dev/apps/resume/src/routes.ts
+++ dev/apps/resume/src/routes.ts
@@ -1,4 +1,4 @@
 import type { RouteConfig } from '@react-router/dev/routes';
 import { index } from '@react-router/dev/routes';
 
-export default [index('routes/home.tsx')] satisfies RouteConfig;
+export default [index('routes/resume.tsx', { id: 'resume' })] satisfies RouteConfig;
