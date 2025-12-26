e957e68c254623b95e459dcfdc30f2a4fe33645b
Aleksi Asikainen <aleksi@abridge.com>
2025-12-26 14:59:56 -0800

    Terraform init

--- ops/infra/main.tf
+++ ops/infra/main.tf
@@ -5,20 +5,15 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "~> 5.15"
     }
   }
 
   backend "s3" {
-    bucket = local.r2_state_bucket_name
-    key    = "terraform/projects/${var.project_name}/envs/${var.env}/terraform.tfstate"
+    bucket = "resume-terraform-state"
+    key    = "terraform/envs/production/terraform.tfstate"
 
-    region    = "auto" # required by backend; not an AWS region in this case
-    endpoints = { s3 = "https://${var.cloudflare_account_id}.r2.cloudflarestorage.com" }
-
-    access_key = var.cloudflare_r2_access_key_id
-    secret_key = var.cloudflare_r2_secret_access_key
-
+    region         = "auto" # required by backend; not an AWS region in this case
     use_path_style = true
 
     # avoid AWS-specific validation/metadata/account calls
--- ops/infra/store.tf
+++ ops/infra/store.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_r2_bucket" "tf_state_bucket" {
   account_id = var.cloudflare_account_id
   name       = local.r2_state_bucket_name
-  location   = "auto"
+  location   = "WNAM"
 }
