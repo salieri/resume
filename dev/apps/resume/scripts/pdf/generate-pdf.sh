35692e767f4df215981fff9b302b10f574980730
Aleksi Asikainen <aleksi.asikainen@gmail.com>
2025-12-30 18:45:20 -0800

    update PDF

--- dev/apps/resume/scripts/pdf/generate-pdf.sh
+++ dev/apps/resume/scripts/pdf/generate-pdf.sh
@@ -5,7 +5,7 @@
 OUT_DIR=$(mktemp -d)
 OUT_FILE="${OUT_DIR}/out.pdf"
 
-./node_modules/.bin/tsx ./scripts/pdf/cli.ts 'http://localhost:3030/' "${OUT_FILE}"
+./node_modules/.bin/tsx ./scripts/pdf/cli.ts --url 'http://localhost:3030/' --out "${OUT_FILE}"
 
 # remove blank pages
 ./scripts/pdf/trim-blank-pages.sh "${OUT_FILE}" './public/aleksi-asikainen-resume.pdf'
